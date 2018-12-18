4b5a5903a79caf87776b63d96523fc327f9505e6
Pablo <pablobrenestec@gmail.com>
2018-12-18 11:52:31 -0600

    Se agrego el archivo de compilación para Linux

---
+++ codeL/Triangle/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mkdir temp
+
+cp Misc/*.* temp
+cp ContextualAnalyzer/*.* temp
+cp SyntacticAnalyzer/*.* temp
+cp CodeGenerator/*.* temp
+
+cd temp
+
+ocamlc -c ErrorReporter.mli
+ocamlc -c RuntimeEntity.mli
+ocamlc -c Ast.mli
+ocamlc -c IdentificationTable.mli
+ocamlc -c TreeDrawer.mli
+ocamlc -c Parser.mli
+ocamlc -c Scanner.mli
+ocamlc -c Checker.mli
+ocamlc -c Encoder.mli
+
+ocamlc -c ErrorReporter.ml
+ocamlc -c IdentificationTable.ml
+ocamlc -c TreeDrawer.ml
+ocamlc -c Parser.ml
+ocamlc -c Scanner.ml
+ocamlc -c Checker.ml
+ocamlc -c Encoder.ml
+ocamlc -c -pp camlp4o Compiler.ml
+
+ocamlc ErrorReporter.cmo IdentificationTable.cmo TreeDrawer.cmo Parser.cmo Scanner.cmo Checker.cmo Encoder.cmo Compiler.cmo -o Triangle
+
+mv Triangle ..
+cd ..
+
+rm -r temp
